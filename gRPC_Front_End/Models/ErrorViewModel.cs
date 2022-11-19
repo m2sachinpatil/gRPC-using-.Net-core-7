6a4bc6ce1c2804d4e6949fa73e2378a2aab594a4
Sachin Patil <emailtosachinpatil@gmail.com>
2022-11-19 15:20:33 -0500

    Removed project
    removed unrelated project

--- gRPC_Front_End/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace gRPC_Front_End.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
